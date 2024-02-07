9e813c37aae32472d0e688ef5da95859e2f6cb1a
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-07 22:38:36 +0530

    03 conversion operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = 0
+let score1 = "33abc"
+let score2 = undefined
+let score3 = true
+let score4 = "ashsih"
+
+let valueInNumber = Number (score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof score);
+console.log(score);
+console.log(typeof (score1));
+console.log(score1);
+console.log(typeof (score2));
+console.log(score2);
+console.log(typeof (score3));
+console.log(score3);
+console.log(typeof (score4));
+console.log(score4);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "" => false
+// "ashish" => true
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

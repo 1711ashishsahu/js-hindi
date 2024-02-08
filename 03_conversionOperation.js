2f886229e36cf186df2719b6f2fe840d256d4ca5
1711ashishsahu <ashishsahu76732@gmail.com>
2024-02-08 08:35:00 +0530

    03 conversion operation

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -5,18 +5,18 @@ let score3 = true
 let score4 = "ashsih"
 
 let valueInNumber = Number (score1)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(typeof score);
-console.log(score);
-console.log(typeof (score1));
-console.log(score1);
-console.log(typeof (score2));
-console.log(score2);
-console.log(typeof (score3));
-console.log(score3);
-console.log(typeof (score4));
-console.log(score4);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof score);
+// console.log(score);
+// console.log(typeof (score1));
+// console.log(score1);
+// console.log(typeof (score2));
+// console.log(score2);
+// console.log(typeof (score3));
+// console.log(score3);
+// console.log(typeof (score4));
+// console.log(score4);
 
 // "33" => 33
 // "33abc" => NaN
@@ -26,9 +26,31 @@ console.log(score4);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "Ashish"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");

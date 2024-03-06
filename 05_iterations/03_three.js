29a6d657e0f7c77fe7cdd67840e55fb160042a44
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-06 22:17:58 +0530

    for of loop

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,36 @@
+// for of
+
+
+// on array
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    if(greet === " "){
+        console.log("continue used!");
+        continue
+    }
+    console.log(`Each char is ${greet}`);
+}
+
+
+// *****************
+// maps
+
+const map = new Map()
+map.set("IN", "India")
+map.set("IN", "India")
+map.set("USA", "United State of America")
+map.set("FR", "France")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+// object don't work on for of

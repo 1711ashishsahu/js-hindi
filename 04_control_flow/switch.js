07eda2f07c634b0447f3679c7726f5c250e72e1a
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-06 07:55:02 +0530

    Switch case statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,60 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("Jan");
+        break
+    case 2:
+        console.log("Feb");
+        break
+    case 3:
+        console.log("March");
+        break
+    case 4:
+        console.log("April");
+        break
+    case 5:
+        console.log("May");
+        break
+    case 6:
+        console.log("June");
+        break
+    default:
+        console.log("default case match");
+        break
+}
+
+const monthInString = "April"
+
+switch(monthInString){
+    case "Jan":
+        console.log("Jan");
+        break
+    case "Feb":
+        console.log("Feb");
+        break
+    case "March":
+        console.log("March");
+        break
+    case "April":
+        console.log("April");
+        break
+    case "May":
+        console.log("May");
+        break
+    case "June":
+        console.log("June");
+        break
+    default:
+        console.log("default case match");
+        break
+}

40263283c6ce6f0f4c6e32816b2c873c1e0d835f
1711ashishsahu <ashishsahu76732@gmail.com>
2024-03-06 17:41:41 +0530

    while or do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,27 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman", "ironman", "spiderman"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`value is ${arr}`);
+    console.log(`value is ${myArray[arr]}`);
+    arr++
+}
+
+
+// do-while loop
+
+let score = 9
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while(score <= 10)
